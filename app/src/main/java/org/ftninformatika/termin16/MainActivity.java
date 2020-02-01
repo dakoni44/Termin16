72d43304f6c958b6c00775079b100dc42ccf8bd4
David Maric <daxy026@gmail.com>
2020-02-01 17:20:53 +0100

    first commit

---
+++ app/src/main/java/org/ftninformatika/termin16/MainActivity.java
@@ -0,0 +1,14 @@
+package org.ftninformatika.termin16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
